63091feace5a69ad06ba2fdf5612dca97e027130
RomainBerthet <berthet.romain3@gmail.com>
2019-04-09 11:43:47 +0200

    Final Commit

--- ESEO_API_REST/src/main/java/com/dao/VilleDAO.java
+++ ESEO_API_REST/src/main/java/com/dao/VilleDAO.java
@@ -2,7 +2,6 @@ package com.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -18,16 +17,12 @@ public class VilleDAO {
 	
 	private static Logger logger = Logger.getLogger(VilleDAO.class.getName());
 	
-	private static String SQL = "SELECT * FROM ville_france WHERE Code_commune_INSEE LIKE '%?%' AND Nom_commune LIKE '%?%' ";
-	
 	private VilleDAO() {
 	}
 
 	public static List<Ville> listeVilles(String insee, String nomVille) throws SQLException {
-		DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-		Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/TWIC?serverTimezone=Australia/Melbourne&user=root&password=");
-		PreparedStatement prepare = connect.prepareStatement(initialisationRequetePreparee(SQL, insee, nomVille), Statement.RETURN_GENERATED_KEYS);
-		ResultSet rst = prepare.executeQuery();
+		Statement stmt = connection();
+		ResultSet rst = stmt.executeQuery("SELECT * FROM ville_france WHERE Code_commune_INSEE LIKE '%"+insee+"%' AND Nom_commune LIKE '%"+nomVille+"%' ORDER BY Nom_commune");
 		List<Ville> villes = new ArrayList<Ville>();	
 		while(rst.next()) {
 			Ville ville = new Ville(rst.getString("Code_commune_INSEE"), rst.getString("Nom_commune"), rst.getString("Code_postal"), rst.getDouble("Latitude"), rst.getDouble("Longitude"));
@@ -50,10 +45,11 @@ public class VilleDAO {
 
 	public static void ajouterModifVille(String insee, String nom, String cp, String lati, String longi) throws SQLException {
 		List<Ville> ville = listeVilles(insee, "");
+		System.out.println(ville.get(0).getNom());
 		Statement stmt = connection();
-		if (ville.size()==1) {
+		if (ville.size()>0) {
 			stmt.executeUpdate("UPDATE ville_france SET Code_commune_INSEE='"+insee+"', Nom_commune='"+nom+"', Code_postal='"+cp+"', Latitude='"+lati+"', Longitude='"+longi+"' WHERE Code_commune_INSEE="+insee);
-		} else if (ville.isEmpty()) {
+		} else {
 			stmt.execute("INSERT INTO ville_france (Code_commune_INSEE, Nom_commune, Code_postal, Latitude, Longitude) VALUES ('"+insee+"', '"+nom+"', '"+cp+"', '"+lati+"', '"+longi+"')");
 			
 		}
@@ -63,23 +59,4 @@ public class VilleDAO {
 		Statement stmt = connection();
 		stmt.execute("DELETE FROM ville_france WHERE Code_commune_INSEE LIKE '"+insee+"'");
 	}
-	
-	/**
-	 * Initialise une requête préparée.
-	 * 
-	 * @param connection la connexion à la BDD.
-	 * @param sql la requête SQL.
-	 * @param returnGeneratedKeys le boolean qui permet de générer des ID ou pas.
-	 * @param objets la liste d'objets à insérer dans la requête.
-	 * @return preparedStatement la requête préparée initialisée.
-	 * @throws SQLException
-	 */
-	protected static String initialisationRequetePreparee(String sql, Object... objets) {
-		String[] listeSQL = (sql+" ").split("\\?");
-		StringBuilder newSQL = new StringBuilder(listeSQL[0]);
-		for(int i = 0; i<objets.length; i++) {
-			newSQL.append("\"" + objets[i] + "\"" + listeSQL[i+1]);
-		}
-		return newSQL.toString().replaceAll("\"null\"", "null");
-	}	
 }
